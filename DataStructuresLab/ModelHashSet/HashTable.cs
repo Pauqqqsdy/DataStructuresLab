2745116127f8a1d25d7dfabf1b66aed4bb97c0c5
Paul <pausdyq@gmail.com>
2025-04-24 00:30:37 +0500

    IEnumerable and Rehashing for HashTable

--- DataStructuresLab/ModelHashSet/HashTable.cs
+++ DataStructuresLab/ModelHashSet/HashTable.cs
@@ -1,35 +1,45 @@
 ﻿using System;
-using System.Drawing;
+using System.Collections;
 using CarLibrary;
 using DataStructuresLab.Model;
 
 namespace DataStructuresLab.ModelHashSet
 {
-    public class HashTable<TKey, TValue>
+    public class HashTable<TKey, TValue> : IEnumerable<Entry<TKey, TValue>>
     {
-        private int defaultLength = 10;
+        private int defaultSize = 10;
+        private double loadFactor = 0.75;
+        private int limitFactor;
         private Item<Entry<TKey, TValue>>[] set;
+        private int count = 0;
 
-        public int Count { get => set.Length; }
+        public int Count => count;
 
         public bool isReadOnly => false;
 
         public HashTable()
         {
-            set = new Item<Entry<TKey, TValue>>[defaultLength];
+            set = new Item<Entry<TKey, TValue>>[defaultSize];
+            limitFactor = (int)(defaultSize * loadFactor);
         }
 
         public void Add(TKey key, TValue value)
         {
             if (key == null)
                 throw new ArgumentNullException(nameof(key), "Ключ не должен быть пустым.");
+            
+            if (count >= limitFactor)
+            {
+                Resize();
+            }
 
-            int index = Math.Abs(key.GetHashCode() % Count);
+            int index = Math.Abs(key.GetHashCode() % set.Length);
             var newEntry = new Entry<TKey, TValue>(key, value);
 
             if (set[index] == null)
             {
                 set[index] = new Item<Entry<TKey, TValue>>(newEntry);
+                count++;
             }
             else
             {
@@ -37,17 +47,48 @@ namespace DataStructuresLab.ModelHashSet
                 while (current.Next != null)
                 {
                     if (current.Data.Key.Equals(key))
-                        return;
+                        throw new ArgumentException(nameof(key), "Элемент с таким ключом уже существует.");
                     current = current.Next;
                 }
                 if (!current.Data.Key.Equals(key))
+                {
                     current.Next = new Item<Entry<TKey, TValue>>(newEntry);
+                    count++;
+                }
+             }
+        }
+
+        private void Resize()
+        {
+            int newSize = set.Length * 2;
+            var newTable = new Item<Entry<TKey, TValue>>[newSize];
+
+            foreach (var entry in this)
+            {
+                int newIndex = Math.Abs(entry.Key.GetHashCode() % newSize);
+                if (newTable[newIndex] == null)
+                {
+                    newTable[newIndex] = new Item<Entry<TKey, TValue>>(entry);
+                }
+                else
+                {
+                    Item<Entry<TKey, TValue>> current = newTable[newIndex];
+                    while (current.Next != null)
+                    {
+                        current = current.Next;
+                    }
+                    current.Next = new Item<Entry<TKey, TValue>>(entry);
+                }
             }
+
+            set = newTable;
+            limitFactor = (int)(newSize * loadFactor);
         }
 
         public void Clear()
         {
-            set = new Item<Entry<TKey, TValue>>[defaultLength];
+            set = new Item<Entry<TKey, TValue>>[defaultSize];
+            count = 0;
         }
 
         public bool Contains(TKey key)
@@ -104,20 +145,40 @@ namespace DataStructuresLab.ModelHashSet
             if (current != null && current.Data.Key.Equals(key))
             {
                 set[index] = current.Next;
+                count--;
                 return true;
             }
 
             while (current != null && current.Next != null)
             {
-                if (current.Next.Data.Equals(key))
+                if (current.Next.Data.Key.Equals(key))
                 {
                     current.Next = current.Next.Next;
+                    count--;
                     return true;
                 }
                 current = current.Next;
             }
 
             return false;
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть пустым.");
+
+            int index = Math.Abs(key.GetHashCode() % set.Length);
+            Item<Entry<TKey, TValue>> current = set[index];
+
+            while (current != null)
+            {
+                if (current.Data.Key.Equals(key))
+                    return current.Data.Value;
+                current = current.Next;
+            }
+
+            throw new KeyNotFoundException("Ключ не найден.");
         }
 
         public void PrintHS()
@@ -139,5 +200,20 @@ namespace DataStructuresLab.ModelHashSet
                 }
             }
         }
+
+        public IEnumerator<Entry<TKey, TValue>> GetEnumerator()
+        {
+            for (int i = 0; i < set.Length; i++)
+            {
+                Item<Entry<TKey, TValue>> current = set[i];
+                while (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
